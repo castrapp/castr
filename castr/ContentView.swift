7b4a6e2f21934a0c6041741221041bf80a774ac8
Harrison Hall <harryson.hall@gmail.com>
2024-08-03 16:40:12 -0400

    Initial Commit

---
+++ castr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  castr
+//
+//  Created by Harrison Hall on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ castr/castrApp.swift
@@ -0,0 +1,17 @@
+//
+//  castrApp.swift
+//  castr
+//
+//  Created by Harrison Hall on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct castrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
